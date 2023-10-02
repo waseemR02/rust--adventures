624b7fd1711307ec9b5be512197d9cf0dea8d278
waseemR02 <waseem.riaz.9999@gmail.com>
2023-10-02 10:43:51 +0530

    Update Cargo.toml

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,21 +2,20 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-
 fn main() {
     println!("Guess the number!");
-    
-    let secret_number = rand::thread_rng().gen_range(1..=10); 
-    
+
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+
     loop {
         println!("Please input your guess.(1,10)");
-        
+
         let mut guess = String::new();
-        
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,67 +1,66 @@
-
 fn loop_type_for() {
-   let a = [10, 20, 30, 40, 50];
+    let a = [10, 20, 30, 40, 50];
 
-   for element in a {
-      println!("the value is: {element}");
-   }
+    for element in a {
+        println!("the value is: {element}");
+    }
 
-   for number in (1..4).rev() {
-      println!("{}!", number);
-   }
-   println!("LIFTOFF!!!");
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
 
 fn loop_type_while() {
-   let mut counter = 3;
+    let mut counter = 3;
 
-   while counter != 0 {
-      println!("count: {counter}");
-      counter -= 1;
-   }
+    while counter != 0 {
+        println!("count: {counter}");
+        counter -= 1;
+    }
 
-   println!("LIFTOFF!!!");
+    println!("LIFTOFF!!!");
 }
 
 fn disambiguating_loop() {
-   let mut count = 0;
+    let mut count = 0;
 
-   'counting_up: loop {
-      println!("counting up: {}", count);
-      
-      let mut remaining = 2;
-      loop {
-         println!("remaining: {remaining}");
-         if remaining == 0 {
-            break;
-         }
-         if count == 5 {
-            break 'counting_up;
-         }
-         remaining -= 1;
-      }
+    'counting_up: loop {
+        println!("counting up: {}", count);
 
-      count += 1;
-   }
+        let mut remaining = 2;
+        loop {
+            println!("remaining: {remaining}");
+            if remaining == 0 {
+                break;
+            }
+            if count == 5 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
 }
 
 fn loop_type_loop() {
-   let mut counter = 1;
-   
-   let result = loop {
-      counter += 1;
+    let mut counter = 1;
 
-      if counter == 10 {
-         break counter * 2;
-      }
-   };
+    let result = loop {
+        counter += 1;
 
-   println!("the result is {result}");
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("the result is {result}");
 }
 
 fn main() {
-   // loop_type_loop(); 
-   // disambiguating_loop();
-   // loop_type_while();
-   loop_type_for();
+    // loop_type_loop();
+    // disambiguating_loop();
+    // loop_type_while();
+    loop_type_for();
 }
