8844a9aa05e51090ac839cc5541fe5fdf3f32fe6
waseemR02 <waseem.riaz.9999@gmail.com>
2024-02-17 16:43:44 +0530

    Define Guess type and define new impl
    Signed-off-by: waseemR02 <waseem.riaz.9999@gmail.com>

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,24 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+#[derive(Debug)]
+pub struct Guess {
+    val: u8,
+}
+
+impl Guess {
+    pub fn new(val: u8) -> Guess {
+        if val < 1 || val > 10 {
+            panic!("Number greater than specified range of the type, got {val}")
+        }
+        Guess { val }
+    }
+
+    pub fn get(&self) -> u8 {
+        self.val
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -16,14 +34,17 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
+            Err(err) => {
+                println!("{err}");
+                continue;
+            }
         };
 
-        println!("You guessed: {guess}");
+        println!("You guessed: {}", guess.get());
 
-        match guess.cmp(&secret_number) {
+        match guess.get().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
