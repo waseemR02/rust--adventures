546aa51b1e96210ec7d919a13c00fc3e8ab2af80
waseemR02 <waseem.riaz.9999@gmail.com>
2024-11-24 23:16:45 +0530

    Grep implementation
    Signed-off-by: waseemR02 <waseem.riaz.9999@gmail.com>

---
+++ minigrep/src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching '{}' in {}...", config.query, config.filepath);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Error with application logic: {e}");
+        process::exit(1);
+    }
+}
